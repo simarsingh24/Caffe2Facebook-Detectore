896c2f727edc46cf76becbcb664a77ebbd659fb3
Harsimar <simarsingh24@gmail.com>
2017-06-01 23:55:16 +0530

    initial commit

---
+++ app/src/main/java/facebook/f8demo/ClassifyCamera.java
@@ -0,0 +1,439 @@
+package facebook.f8demo;
+
+import android.Manifest;
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.PixelFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.util.Size;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static android.view.View.SYSTEM_UI_FLAG_IMMERSIVE;
+
+public class ClassifyCamera extends AppCompatActivity {
+    private static final String TAG = "F8DEMO";
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+
+    private static int w =0 ;
+    private static int h =0 ;
+    private static int rowStride ;
+    private static int pixelStride;
+    private static byte[] Y ;
+    private static  byte[] U ;
+    private static byte[] V ;
+
+    private TextureView textureView;
+    private String cameraId;
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession cameraCaptureSessions;
+    protected CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimension;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+    private TextView tv;
+    private String predictedClass = "none";
+    private AssetManager mgr;
+    private boolean processing = false;
+    private Image image = null;
+    private boolean run_HWC = false;
+    private static boolean buttonPressed=false;
+    private static  SurfaceTexture texture;
+    private static ImageReader reader;
+    private static Button capture;
+    private static int[] mRgbBuffer;
+    private static ImageView captureView;
+
+    private static ImageReader.OnImageAvailableListener readerListener;
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public native String classificationFromCaffe2(int h, int w, byte[] Y, byte[] U, byte[] V,
+                                                  int rowStride, int pixelStride, boolean r_hwc);
+
+    public native void initCaffe2(AssetManager mgr);
+
+
+    private class SetUpNeuralNetwork extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void[] v) {
+            try {
+                initCaffe2(mgr);
+                predictedClass = "Neural net loaded! Inferring...";
+            } catch (Exception e) {
+                Log.d(TAG, "Couldn't load neural network.");
+            }
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        mgr = getResources().getAssets();
+
+        new SetUpNeuralNetwork().execute();
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        setContentView(R.layout.activity_classify_camera);
+
+        captureView=(ImageView)findViewById(R.id.captured_image);
+        textureView = (TextureView) findViewById(R.id.textureView);
+        textureView.setSystemUiVisibility(SYSTEM_UI_FLAG_IMMERSIVE);
+        capture=(Button)findViewById(R.id.capture_btn);
+
+        mRgbBuffer=new int[(textureView.getWidth() * textureView.getHeight())*3];
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getInfernce();
+
+            }
+        });
+
+        final GestureDetector gestureDetector = new GestureDetector(this.getApplicationContext(),
+                new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                super.onLongPress(e);
+
+            }
+
+            @Override
+            public boolean onDoubleTapEvent(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+        });
+
+        textureView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
+
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(textureListener);
+        tv = (TextView) findViewById(R.id.sample_text);
+
+    }
+
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            //open your camera here
+            openCamera();
+        }
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            // Transform you image captured size according to the surface width and height
+        }
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        }
+    };
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            cameraDevice = camera;
+            createCameraPreview();
+        }
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            cameraDevice.close();
+        }
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+    protected void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("Camera Background");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+    protected void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getInfernce() {
+
+        predictedClass = classificationFromCaffe2(h, w, Y, U, V,
+                rowStride, pixelStride, run_HWC);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tv.setText(predictedClass);
+            }
+        });
+
+    }
+    private void getRGBIntFromPlanes(Image.Plane[] planes) {
+        ByteBuffer yPlane = planes[0].getBuffer();
+        ByteBuffer uPlane = planes[1].getBuffer();
+        ByteBuffer vPlane = planes[2].getBuffer();
+
+        int bufferIndex = 0;
+        final int total = yPlane.capacity();
+        final int uvCapacity = uPlane.capacity();
+        final int width = planes[0].getRowStride();
+
+        int yPos = 0;
+        for (int i = 0; i < 227; i++) {
+            int uvPos = (i >> 1) * width;
+
+            for (int j = 0; j < width; j++) {
+                if (uvPos >= uvCapacity-1)
+                    break;
+                if (yPos >= total)
+                    break;
+
+                final int y1 = yPlane.get(yPos++) & 0xff;
+
+                final int u = (uPlane.get(uvPos) & 0xff) - 128;
+                final int v = (vPlane.get(uvPos) & 0xff) - 128;
+                if ((j & 1) == 1) {
+                    uvPos += 2;
+                }
+
+                final int y1192 = 1192 * y1;
+                int r = (y1192 + 1634 * v);
+                int g = (y1192 - 833 * v - 400 * u);
+                int b = (y1192 + 2066 * u);
+
+                r = (r < 0) ? 0 : ((r > 262143) ? 262143 : r);
+                g = (g < 0) ? 0 : ((g > 262143) ? 262143 : g);
+                b = (b < 0) ? 0 : ((b > 262143) ? 262143 : b);
+
+                mRgbBuffer[bufferIndex++] = ((r << 6) & 0xff0000) |
+                        ((g >> 2) & 0xff00) |
+                        ((b >> 10) & 0xff);
+            }
+
+        }
+        final Bitmap bitmap=Bitmap.createBitmap(mRgbBuffer,227,227,Bitmap.Config.RGB_565);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                captureView.setImageBitmap(bitmap);
+
+            }
+        });
+
+    }
+    protected void createCameraPreview() {
+        try {
+            texture  = textureView.getSurfaceTexture();
+            assert texture != null;
+            texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
+            Surface surface = new Surface(texture);
+            int width = 227;
+            int height = 227;
+            reader = ImageReader.newInstance(width, height, ImageFormat.YUV_420_888, 4);
+            readerListener = new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    try {
+
+                        image = reader.acquireNextImage();
+                        if (processing) {
+                            image.close();
+                            return;
+                        }
+                        processing = true;
+                        w = image.getWidth();
+                        h = image.getHeight();
+                        ByteBuffer Ybuffer = image.getPlanes()[0].getBuffer();
+                        ByteBuffer Ubuffer = image.getPlanes()[1].getBuffer();
+                        ByteBuffer Vbuffer = image.getPlanes()[2].getBuffer();
+                        // TODO: use these for proper image processing on different formats.
+                        rowStride = image.getPlanes()[1].getRowStride();
+                        pixelStride = image.getPlanes()[1].getPixelStride();
+                        Y = new byte[Ybuffer.capacity()];
+                        U = new byte[Ubuffer.capacity()];
+                        V = new byte[Vbuffer.capacity()];
+                        Ybuffer.get(Y);
+                        Ubuffer.get(U);
+                        Vbuffer.get(V);
+
+                        final Image.Plane[] planes = image.getPlanes();
+                        final int total = planes[0].getRowStride() * 227;
+                        if (mRgbBuffer == null || mRgbBuffer.length < total)
+                            mRgbBuffer = new int[total];
+
+                        processing = false;
+
+                        getRGBIntFromPlanes(planes);
+                          /*  predictedClass = classificationFromCaffe2(h, w, Y, U, V,
+                                    rowStride, pixelStride, run_HWC);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tv.setText(predictedClass);
+                                    processing = false;
+                                }
+                            });*/
+                    } finally {
+                        if (image != null) {
+                            image.close();
+                        }
+                    }
+                }
+            };
+            reader.setOnImageAvailableListener(readerListener, mBackgroundHandler);
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureRequestBuilder.addTarget(surface);
+            captureRequestBuilder.addTarget(reader.getSurface());
+
+            cameraDevice.createCaptureSession(Arrays.asList(surface, reader.getSurface()), new CameraCaptureSession.StateCallback(){
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    if (null == cameraDevice) {
+                        return;
+                    }
+                    cameraCaptureSessions = cameraCaptureSession;
+                    updatePreview();
+                }
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    Toast.makeText(ClassifyCamera.this, "Configuration change", Toast.LENGTH_SHORT).show();
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void openCamera() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = manager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            assert map != null;
+            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(ClassifyCamera.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CAMERA_PERMISSION);
+                return;
+            }
+            manager.openCamera(cameraId, stateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void updatePreview() {
+        if(null == cameraDevice) {
+            return;
+        }
+        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        try {
+            cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (null != cameraDevice) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(ClassifyCamera.this, "You can't use this app without granting permission", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startBackgroundThread();
+        if (textureView.isAvailable()) {
+            openCamera();
+        } else {
+            textureView.setSurfaceTextureListener(textureListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        closeCamera();
+        stopBackgroundThread();
+        super.onPause();
+    }
+}
