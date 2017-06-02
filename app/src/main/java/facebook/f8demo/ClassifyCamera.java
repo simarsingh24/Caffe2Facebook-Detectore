3febcdde8874ad50b1d0e5ca60e8389453516800
Harsimar <simarsingh24@gmail.com>
2017-06-02 11:37:51 +0530

    ui improvement

--- app/src/main/java/facebook/f8demo/ClassifyCamera.java
+++ app/src/main/java/facebook/f8demo/ClassifyCamera.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.graphics.Matrix;
 import android.graphics.PixelFormat;
@@ -292,6 +293,67 @@ public class ClassifyCamera extends AppCompatActivity {
         });
 
     }
+    private void toRGB565(byte[] yuvs, int width, int height, byte[] rgbs) {
+        //the end of the luminance data
+        final int lumEnd = width * height;
+        //points to the next luminance value pair
+        int lumPtr = 0;
+        //points to the next chromiance value pair
+        int chrPtr = lumEnd;
+        //points to the next byte output pair of RGB565 value
+        int outPtr = 0;
+        //the end of the current luminance scanline
+        int lineEnd = width;
+
+        while (true) {
+
+            //skip back to the start of the chromiance values when necessary
+            if (lumPtr == lineEnd) {
+                if (lumPtr == lumEnd) break; //we've reached the end
+                //division here is a bit expensive, but's only done once per scanline
+                chrPtr = lumEnd + ((lumPtr  >> 1) / width) * width;
+                lineEnd += width;
+            }
+
+            //read the luminance and chromiance values
+            final int Y1 = yuvs[lumPtr++] & 0xff;
+            final int Y2 = yuvs[lumPtr++] & 0xff;
+            final int Cr = (yuvs[chrPtr++] & 0xff) - 128;
+            final int Cb = (yuvs[chrPtr++] & 0xff) - 128;
+            int R, G, B;
+
+            //generate first RGB components
+            B = Y1 + ((454 * Cb) >> 8);
+            if(B < 0) B = 0; else if(B > 255) B = 255;
+            G = Y1 - ((88 * Cb + 183 * Cr) >> 8);
+            if(G < 0) G = 0; else if(G > 255) G = 255;
+            R = Y1 + ((359 * Cr) >> 8);
+            if(R < 0) R = 0; else if(R > 255) R = 255;
+            //NOTE: this assume little-endian encoding
+            rgbs[outPtr++]  = (byte) (((G & 0x3c) << 3) | (B >> 3));
+            rgbs[outPtr++]  = (byte) ((R & 0xf8) | (G >> 5));
+
+            //generate second RGB components
+            B = Y2 + ((454 * Cb) >> 8);
+            if(B < 0) B = 0; else if(B > 255) B = 255;
+            G = Y2 - ((88 * Cb + 183 * Cr) >> 8);
+            if(G < 0) G = 0; else if(G > 255) G = 255;
+            R = Y2 + ((359 * Cr) >> 8);
+            if(R < 0) R = 0; else if(R > 255) R = 255;
+            //NOTE: this assume little-endian encoding
+            rgbs[outPtr++]  = (byte) (((G & 0x3c) << 3) | (B >> 3));
+            rgbs[outPtr++]  = (byte) ((R & 0xf8) | (G >> 5));
+            final Bitmap bitmap= BitmapFactory.decodeByteArray(rgbs,0,227);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    captureView.setImageBitmap(bitmap);
+
+                }
+            });
+
+        }
+    }
     protected void createCameraPreview() {
         try {
             texture  = textureView.getSurfaceTexture();
@@ -333,8 +395,7 @@ public class ClassifyCamera extends AppCompatActivity {
                             mRgbBuffer = new int[total];
 
                         processing = false;
-
-                        getRGBIntFromPlanes(planes);
+                        //getRGBIntFromPlanes(planes);
                           /*  predictedClass = classificationFromCaffe2(h, w, Y, U, V,
                                     rowStride, pixelStride, run_HWC);
                             runOnUiThread(new Runnable() {
